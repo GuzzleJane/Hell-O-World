81ebabfaf6da75bd748745b5197a121822e8bbcb
GuzzleJane <76995700+GuzzleJane@users.noreply.github.com>
2021-01-05 15:05:30 +0530

    F**k You PHP

---
+++ php_sucks.py
@@ -0,0 +1,3 @@
+Me_from_inside = "Hello Dear God, Please Save Me From This F**king World"
+Me_from_outside = Me_from_inside.replace(Me_from_inside[5:len(Me_from_inside)-5]," ")
+print(Me_from_outside)
